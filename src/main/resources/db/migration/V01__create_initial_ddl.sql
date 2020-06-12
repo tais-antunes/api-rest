61c3e4d5f72c3c6daa9c7b5e7d6ccd24d3a64960
Tais Antunes <tais.antunes@compasso.com.br>
2020-06-12 09:53:44 -0300

    correção enum no ddl

--- src/main/resources/db/migration/V01__create_initial_ddl.sql
+++ src/main/resources/db/migration/V01__create_initial_ddl.sql
@@ -1,7 +1,7 @@
 CREATE TABLE cliente (
 	id 		INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	nome_completo 	VARCHAR(255) NOT NULL,
-	sexo 		EMUM('M','F') NOT NULL,
+	sexo 		ENUM('M','F') NOT NULL,
 	data_nascimento DATE NOT NULL, 
 	idade 		SMALLINT NOT NULL,
 	cidade_id 	INT NOT NULL
@@ -12,7 +12,7 @@ CREATE TABLE cliente (
 CREATE TABLE cidade (
 	id 	    INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	nome_cidade VARCHAR(255) NOT NULL,
-	estado	    ENUM(AC, 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO') NOT NULL
+	estado	    ENUM('AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO') NOT NULL
 
 ) ENGINE=InnoDB;
 
